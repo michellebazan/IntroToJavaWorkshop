1df5d600b445849c8d08f22811b3eceb25e35bbe
michellebazan <33267418+michellebazan@users.noreply.github.com>
2017-12-06 15:00:43 -0800

    Add files via upload

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,20 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+	Robot soul = new Robot();
+	soul.setSpeed(100);
+	soul.penDown();
+	soul.setPenWidth(10);
+	soul.setRandomPenColor();/// can't choose color
+soul.sparkle();
+Robot.setWindowColor(20, 0, 0);
+
+for(int i = 0; i < 4; i++) {
+		soul.move(250);
+		soul.turn(90);
+}
+	soul.setPenColor(Color.orange);///can choose color
+	soul.turn(90);
+soul.move(250);
 	}
 }
--- src/day2/Spiral.java
+++ src/day2/Spiral.java
@@ -6,11 +6,11 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot rasengan = new Robot();
 		// 5. Set your robot's pen to the down position
 		
 		// 3. Set the robot to go at max speed (10)
-		
+		rasengan.setSpeed(10);
 		// 4. Do the following (steps 6-8) 75 times
 		
 			// 7. Change the pen color to random
@@ -18,7 +18,7 @@ public class Spiral {
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
 			
 			// 2. Turn the robot 1/7 of 360 degrees to the right
-		
+		rasengan.turn(51);
 			// 8. Set the pen width to i
 			
 	}
